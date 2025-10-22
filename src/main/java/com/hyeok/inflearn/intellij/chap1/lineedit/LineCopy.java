d8f568b80fe4a7b3ecea59680e182f3601c7cf8f
jaehyeok-99 <wjklop4569@naver.com>
2025-10-23 00:05:13 +0900

    라인 수정하기_251023

---
+++ src/main/java/com/hyeok/inflearn/intellij/chap1/lineedit/LineCopy.java
@@ -0,0 +1,9 @@
+package com.hyeok.inflearn.intellij.chap1.lineedit;
+
+public class LineCopy {
+
+    public void copyLine() {
+        System.out.println("현재 줄을 복사합니다");
+        System.out.println("현재 줄을 복사합니다");
+    }
+}
